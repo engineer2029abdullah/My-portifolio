5d375fc89552ce24b59ca7f4332bc0618f7bec4f
engineer2029abdullah <abdullah.4250099@eng.modern-academy.edu.eg>
2025-12-26 06:07:55 +0200

    Add files via upload

---
+++ assets/data.js
@@ -0,0 +1,9 @@
+export const stats = {
+  totalEmployes: 1500,
+  totalTeams: 3050,
+  totalProcessTasks: 504,
+  totalCompletedTasks: 135,
+};
+
+export const chartGrowth = [10, 15, 18, 27, 31];
+export const chartLabels = ["Jan", "Feb", "Mar", "Apr", "May"];
